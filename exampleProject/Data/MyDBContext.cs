e9ada66da1002120a69bcc12b5ebc2820ce065dd
bonya0329 <60532283+bonya0329@users.noreply.github.com>
2020-04-25 02:36:48 +0600

    Добавьте файлы проекта.

---
+++ exampleProject/Data/MyDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using exampleProject.Models;
+
+namespace exampleProject.Data
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext (DbContextOptions<MyDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<exampleProject.Models.Student> Student { get; set; }
+    }
+}
---
+++ exampleProject/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exampleProject.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+    }
+}
